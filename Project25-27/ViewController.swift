0d43d6ea1a99f0e9093afa8c66936a9d95082b2b
Eddie Jung <ejung716@gmail.com>
2021-09-09 09:51:47 -0700

    Add collectionViewController and storyboard.

--- Project25-27/ViewController.swift
+++ Project25-27/ViewController.swift
@@ -7,11 +7,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // TODO
+        /*
+         1. create collectionview revised project 103c
+         2. create detail view with imageView, 3 buttons
+         3. add image alerts , image controller, image picker project101112 or project13
+         4. render image with updated text in positions proj 27 ch 3 and share image
+         5. add share
+         */
     }
 
 
